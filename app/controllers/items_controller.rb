2f5c025fc045948217847ea52eef13a7cb57824c
mayachacha <asakawa926@gmail.com>
2023-08-14 16:16:47 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,26 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new]
 
 def index
+end
+
+def new
+  @item = Item.new
+end
+
+def create
+  @item = Item.new(item_params)
+  if @item.image.attached? && @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+
+private
+def item_params
+params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+
 end
 end
