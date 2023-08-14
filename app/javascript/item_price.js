496ac014a9c1375496b8677c8c4236c0d96f6e28
mayachacha <asakawa926@gmail.com>
2023-08-14 19:27:27 +0900

    javascript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,15 @@
 function calculation (){
   const priceInput = document.getElementById("item-price");
-  const taxPrice = document.getElementById("add-tax-price");
+  const addTaxDom = document.getElementById("add-tax-price");
   const profitPrice = document.getElementById("profit");
 
-  priceInput.addEventListener("keyup", () => {
+  priceInput.addEventListener("input", () => {
     const price = parseFloat(priceInput.value);
     const tax = Math.floor(price * 0.1);
     const profit = price - tax;
 
-    taxPrice.textContent = tax; // 修正
-    profitPrice.textContent = profit; // 修正
+    addTaxDom.innerHTML = tax;
+    profitPrice.innerHTML= profit;
   });
 }
 
