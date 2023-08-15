4d168edc8957039d9dd8c1548d99e54665156803
mayachacha <139746535+mayachacha@users.noreply.github.com>
2023-08-15 17:34:20 +0900

    Revert "商品編集機能の実装"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new]
 
 def index
   @items = Item.includes(:user).order(created_at: :desc)
@@ -20,22 +20,6 @@ end
 
 def show
   @item = Item.find(params[:id])
-end
-
-def edit
-  @item = Item.find(params[:id])
-  unless current_user.id == @item.user_id
-  redirect_to root_path
-  end
-end
-
-def update
-  @item = Item.find(params[:id])
-  if @item.update(item_params)
-    redirect_to item_path(@item.id)
-  else
-    render :edit, status: :unprocessable_entity
-  end
 end
 
 
