8afcecb510d21a33c15884258a2726cfe13ee06e
mayachacha <asakawa926@gmail.com>
2023-08-15 19:43:33 +0900

    コントローラー条件分岐修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
 def index
   @items = Item.includes(:user).order(created_at: :desc)
 end
@@ -41,6 +42,8 @@ def destroy
   if user_signed_in? && current_user.id == item.user_id
     item.destroy
     redirect_to root_path
+  else
+    redirect_to item_path(item.id)
   end
 end
 
