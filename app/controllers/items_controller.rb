f53a64cd3a7aed6909982dea2df2bbb3a7780619
mayachacha <asakawa926@gmail.com>
2023-08-15 18:05:43 +0900

    "プライベートメソッドの設定"
    This reverts commit 05c4507272d8600c0743b1744e28e7e5c8931925.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-
+  before_action :authenticate_user!, only: [:new, :edit]
+  before action :set_item, only: [:show, :edit, :update]
 def index
   @items = Item.includes(:user).order(created_at: :desc)
 end
@@ -19,15 +19,33 @@ def create
 end
 
 def show
-  @item = Item.find(params[:id])
+
+end
+
+def edit  
+  unless current_user.id == @item.user_id
+  redirect_to root_path
+  end
+end
+
+def update
+  if @item.update(item_params)
+    redirect_to item_path(@item.id)
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
 
 
 private
 def item_params
 params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
-
 end
+
+def set_item
+  @item = Item.find(params[:id])
+end
+
 end
 
 #  @item.image.attached? &&
