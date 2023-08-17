375dd05a7b9adb1a15393a433211e43eebfa33da
mayachacha <asakawa926@gmail.com>
2023-08-17 19:34:14 +0900

    ビューの修正、商品購入機能修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,7 +6,6 @@ FactoryBot.define do
     street_address    { '1-1' }
     building          { '東京ハイツ' }
     phone_number      { Faker::Number.number(digits: rand(10..11)) }
-    price             {6000}
     token             {"tok_abcdefghijk00000000000000000"}
   end
 end
